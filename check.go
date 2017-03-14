9a31c511caf47b265b9bd5679d4a933da57c9300
Sebastian Mancke <s.mancke@tarent.de>
2017-03-14 09:14:49 +0100

    changed the result type to be a struct

--- check.go
+++ check.go
@@ -13,13 +13,13 @@ var emailRexp = regexp.MustCompile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]
 // Check checks the syntax and if valid, it checks the mailbox by connecting to
 // the target mailserver
 // The fromEmail is used as from address in the communication to the foreign mailserver.
-func Check(fromEmail, checkEmail string) (result CheckResult, textMessage string, err error) {
+func Check(fromEmail, checkEmail string) (result Result, err error) {
 	if !CheckSyntax(checkEmail) {
-		return Invalid, "invalid syntax", nil
+		return InvalidSyntax, nil
 	}
 
 	if CheckDisposable(checkEmail) {
-		return Disposable, "disposable email", nil
+		return Disposable, nil
 	}
 	return CheckMailbox(fromEmail, checkEmail)
 }
@@ -31,15 +31,16 @@ func CheckSyntax(checkEmail string) bool {
 
 // CheckMailbox checks the checkEmail by connecting to the target mailbox and returns the result.
 // The fromEmail is used as from address in the communication to the foreign mailserver.
-func CheckMailbox(fromEmail, checkEmail string) (result CheckResult, textMessage string, err error) {
+func CheckMailbox(fromEmail, checkEmail string) (result Result, err error) {
 	mxList, err := net.LookupMX(hostname(checkEmail))
+	// TODO: Distinguish between usual network errors
 	if err != nil || len(mxList) == 0 {
-		return Invalid, "error, no mailserver for hostname", nil
+		return InvalidDomain, nil
 	}
 	return checkMailbox(fromEmail, checkEmail, mxList, 25)
 }
 
-func checkMailbox(fromEmail, checkEmail string, mxList []*net.MX, port int) (result CheckResult, textMessage string, err error) {
+func checkMailbox(fromEmail, checkEmail string, mxList []*net.MX, port int) (result Result, err error) {
 	// try to connect to one mx
 	var c *smtp.Client
 	for _, mx := range mxList {
@@ -49,7 +50,7 @@ func checkMailbox(fromEmail, checkEmail string, mxList []*net.MX, port int) (res
 		}
 	}
 	if err != nil {
-		return Undefined, "smtp error", err
+		return MailserverError, err
 	}
 	defer c.Close()
 	defer c.Quit() // defer ist LIFO
@@ -57,32 +58,32 @@ func checkMailbox(fromEmail, checkEmail string, mxList []*net.MX, port int) (res
 	// HELO
 	err = c.Hello(hostname(fromEmail))
 	if err != nil {
-		return Undefined, "smtp error", err
+		return MailserverError, err
 	}
 
 	// MAIL FROM
 	err = c.Mail(fromEmail)
 	if err != nil {
-		return Undefined, "smtp error", err
+		return MailserverError, err
 	}
 
 	// RCPT TO
 	id, err := c.Text.Cmd("RCPT TO:<%s>", checkEmail)
 	if err != nil {
-		return Undefined, "smtp error", err
+		return MailserverError, err
 	}
 	c.Text.StartResponse(id)
-	code, msg, err := c.Text.ReadResponse(25)
+	code, _, err := c.Text.ReadResponse(25)
 	c.Text.EndResponse(id)
 	if code == 550 {
-		return Invalid, "mailbox unavailable", nil
+		return MailboxUnavailable, nil
 	}
 
 	if err != nil {
-		return Undefined, msg, err
+		return MailserverError, err
 	}
 
-	return Valid, "Ok", nil
+	return Valid, nil
 }
 
 func hostname(mail string) string {
--- check_result.go
+++ check_result.go
@@ -8,11 +8,11 @@ type Result struct {
 }
 
 var (
-	Valid                 = Result{"valid", "mailboxChecked", "The mail address is valid."}
-	InvalidSyntax         = Result{"invalid", "invalidSyntax", "The format is invalid."}
-	InvalidDomain         = Result{"invalid", "invalidDomain", "The domain does not exist."}
-	MailboxUnavailable    = Result{"invalid", "mailboxUnavailable", "The username does not exist."}
-	Disposable            = Result{"invalid", "disposable", "The mailserver is a throw-away mail gateway."}
-	DetailMailserverError = Result{"error", "mailserverError", "The target mailserver responded with an error."}
-	DetailServiceError    = Result{"error", "serviceError", "An internal error occured while checking."}
+	Valid              = Result{"valid", "mailboxChecked", "The mail address is valid."}
+	InvalidSyntax      = Result{"invalid", "invalidSyntax", "The format is invalid."}
+	InvalidDomain      = Result{"invalid", "invalidDomain", "The domain does not exist."}
+	MailboxUnavailable = Result{"invalid", "mailboxUnavailable", "The username does not exist."}
+	Disposable         = Result{"invalid", "disposable", "The mailserver is a throw-away mail gateway."}
+	MailserverError    = Result{"error", "mailserverError", "The target mailserver responded with an error."}
+	ServiceError       = Result{"error", "serviceError", "An internal error occured while checking."}
 )
